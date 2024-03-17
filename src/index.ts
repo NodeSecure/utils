29a3385195f20e7bc142590f85fd4851c5ba4c52
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2024-03-17 15:15:26 +0100

    feat: add scorecard utils (#79)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./formatBytes.js";
 export * from "./locationToString.js";
 export * from "./taggedString.js";
 export * from "./parseManifestAuthor.js";
+export * from "./scorecard.js";
--- src/taggedString.ts
+++ src/taggedString.ts
@@ -1,15 +1,15 @@
-export function taggedString(chaines: TemplateStringsArray, ...cles: (number | string)[]) {
-  return function cur(...valeurs: (string | number | {[key: string]: any})[]) {
-    const lastVal = valeurs[valeurs.length - 1];
+export function taggedString(strings: TemplateStringsArray, ...keys: (number | string)[]) {
+  return function cur(...values: (string | number | {[key: string]: any})[]) {
+    const lastVal = values[values.length - 1];
     const dict = typeof lastVal === "object" && lastVal !== null ? lastVal : {};
-    const resultat = [chaines[0]];
-    cles.forEach((cle, index) => {
-      resultat.push(
-        typeof cle === "number" ? valeurs[cle] : dict[cle],
-        chaines[index + 1]
+    const result = [strings[0]];
+    keys.forEach((key, index) => {
+      result.push(
+        typeof key === "number" ? values[key] : dict[key],
+        strings[index + 1]
       );
     });
 
-    return resultat.join("");
+    return result.join("");
   };
 }
